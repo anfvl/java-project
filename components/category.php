01d3ab01fd5ccb9064bea4eee2a0c19af3e896b4
anfvl <anfelkadri02@gmail.com>
2025-04-17 16:58:26 +0200

    1st commit

--- components/category.php
+++ components/category.php
@@ -1,6 +1,7 @@
 <div class="category">
     <img src='../assets/slides/collection.jpg' alt='class image'>
     <?php
+    
     $class = $_GET['class'];
     echo "<div class='category-name'>$class</div>";
     ?>
