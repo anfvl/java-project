4c14303a5e29321d60159b168819d7e9aa6c012e
Amir hattab <146218666+AmirtterK@users.noreply.github.com>
2025-05-26 20:44:29 +0200

    Delete components directory

--- components/category.php
+++
@@ -1,9 +0,0 @@
-<div class="category">
-    <img src='../assets/slides/collection.jpg' alt='class image'>
-    <?php
-    
-    $class = $_GET['class'];
-    echo "<div class='category-name'>$class</div>";
-    ?>
-
-</div>
